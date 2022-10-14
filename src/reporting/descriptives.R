5d6fd55e62abcd014ff8c85b3bbc183ef8b90c2b
indiwieggers123 <i.wieggers@tilburguniversity.edu>
2022-10-14 20:31:11 +0200

    added descriptives in R

---
+++ src/reporting/descriptives.R
@@ -0,0 +1,19 @@
+library(readr)
+library(dplyr)
+
+setwd("~/Documents/Master/Blok 1/ODCM diets/recipe-nutritions/data")
+Edamam_data <- read_csv('All_diets.csv')
+
+View(Edamam_data)
+summary(Edamam_data)
+head(Edamam_data)
+tail(Edamam_data)
+
+ncol(Edamam_data)
+nrow(Edamam_data)
+
+Edamam_data %>% 
+    group_by(Diet_type) %>% 
+    summarize(meanProtein = mean(`Protein(g)`),
+              meanFat = mean(`Fat(g)`),
+              meanCarbs = mean(`Carbs(g)`))
